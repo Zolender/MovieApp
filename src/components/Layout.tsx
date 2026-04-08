f27d5778e2935ff79740dce1be3c88f2ece098fe
Zolender <ndeingare@gmail.com>
2026-04-08 10:40:11 +0200

    feat: implement Layout and ProtectedRoute components with basic structure

---
+++ src/components/Layout.tsx
@@ -0,0 +1,7 @@
+const Layout = () => {
+    return (
+        <>Layout Page</>
+    );
+}
+
+export default Layout;
---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,7 @@
+const ProtectedRoute = () => {
+    return (
+        <>Protected route page</>
+    );
+}
+ 
+export default ProtectedRoute;
