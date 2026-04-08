f7f2c732f005ceeda68296eeee7e02455e348468
Zolender <ndeingare@gmail.com>
2026-04-08 10:58:27 +0200

    feat: implement authSlice with login and logout methods and localstorage utilisation

---
+++ src/store/authSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import type { User } from "../types"
+
+type AuthState = {
+    currentUser: User | null
+}
+
+const storedUser = localStorage.getItem("currentUser")
+
+const initialState : AuthState = {
+    currentUser : storedUser? JSON.parse(storedUser): null
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers:{
+        loginUser: (state, action: PayloadAction<User>)=>{
+            state.currentUser = action.payload
+        },
+        logoutUser: (state)=>{
+            state.currentUser = null
+            localStorage.removeItem("currentUser")
+        }
+    }
+})
+
+
+export const {loginUser, logoutUser} = authSlice.actions
+export default authSlice.reducer
