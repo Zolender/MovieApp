29a98c046abcfa2513957623ee220ff1fe1eca23
Zolender <ndeingare@gmail.com>
2026-04-12 21:15:46 +0200

    feat: implement Search component with movie search functionality

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import { SearchResponse } from "../types";
+
+const apiKey = import.meta.env.API_URL
+
 const Search = () => {
-    const apiKey = import.meta.env.API_URL
+
+    const navigate = useNavigate()
+    const [query, setQuery] =  useState("")
+    const [searchUrl, setSearchUrl] = useState("")
+    const {data, isLoading, error} = useFetch<SearchResponse>(searchUrl)
+
+    const handleSubmit= (e: React.SubmitEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+        if(!query.trim())return
+        setSearchUrl(`https://www.omdabpi.com/?s=${encodeURIComponent(query)}&apikey=${apiKey}`)
+    }
+    
     return (
-        <>Search Page</>
+        <div className="">
+            <h1 className="">Search Movies</h1>
+            <form className="">
+                <input type="text" value={query} onChange={(e)=> setQuery(e.target.value)} placeholder="Search a movie..." className="" />
+                <button className="" disabled={isLoading} type="submit">{isLoading? "Searching...": "Search"}</button>
+            
+            </form>
+        </div>
     );
 }
  
