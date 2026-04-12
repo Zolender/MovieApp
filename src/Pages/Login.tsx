61db76e2b5e6c9da46a3124ecc17b658f260df16
Zolender <ndeingare@gmail.com>
2026-04-12 20:30:16 +0200

    feat: improve Login component UI with enhanced styling and layout

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -34,23 +34,33 @@ const Login = () => {
     }
 
     return (
-        <div className="">
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" name="username" id="username" className="" onChange={handleChange} value={formData.username} placeholder="Enter your name"/>
-                </div>
+        <div className="flex items-center justify-center h-screen">
+            <div className="bg-white p-8 rounded-md shadow-md">
+                <h1 className="text-2xl font-semibold">Login</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="space-y-4">
+                        <div>
+                            <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
+                                Username
+                            </label>
+                            <input type="text" name="username" id="username" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" onChange={handleChange} value={formData.username} placeholder="Enter your name"/>
+                        </div>
 
-                <div className="">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="Enter a password" value={formData.password} onChange={handleChange} className="" name="password" id="password" />
-                </div>
+                        <div>
+                            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+                                Password
+                            </label>
+                            <input type="password" placeholder="Enter a password" value={formData.password} onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="password" id="password" />
+                        </div>
 
-                {error && <p className="text-red-400">{error}</p>}
+                        {error && <p className="text-red-400">{error}</p>}
 
-                <button type="submit">Login</button>
-            </form>
+                        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
