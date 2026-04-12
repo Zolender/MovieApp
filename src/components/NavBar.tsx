934da72adeec977ac6e199f2417c46cde8bcdd47
Zolender <ndeingare@gmail.com>
2026-04-12 20:48:19 +0200

    feat: implement NavBar component with navigation links and user logout functionality

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Link, replace, useNavigate } from "react-router-dom";
+import { RootState } from "../store/store";
+import { logoutUser } from "../store/authSlice";
+
 const NavBar = () => {
+
+    const dispatch = useDispatch()
+    const currentUser = useSelector((state: RootState)=> state.auth.currentUser)
+    const navigate = useNavigate()
+
+    function handleLogout(){
+        dispatch(logoutUser())
+        navigate("/login", {replace: true})
+
+    }
+
     return (
-        <>NavBar</>
+        <nav className="">
+            <div className="">
+                <Link to={"/search"}>Search</Link>
+                <Link to={"/favorites"}>Favorites</Link>
+            </div>
+            <p className="">Hello! Welcome, <span className="">{currentUser?.username || "User"}</span></p>
+            <button className="" onClick={handleLogout}>Logout</button>
+        </nav>
     );
 }
  
