e105feff8f5719e4bb24612a5e6d1b6bbf1f5ed1
Zolender <ndeingare@gmail.com>
2026-04-12 21:35:57 +0200

    feat: implement MovieDetails component to fetch and display movie information

--- src/Pages/MovieDetails.tsx
+++ src/Pages/MovieDetails.tsx
@@ -1,6 +1,28 @@
+import { useNavigate, useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import { MovieDetails } from "../types";
+
+const apiKey = import.meta.env.API_URL
+
 const MovieDetails = () => {
+    const {id}= useParams();
+    const navigate = useNavigate()
+    const {data: movie, isLoading, error} = useFetch<MovieDetails>(`https://www.omdbapi.com/?i=${id}&apikey=${apiKey}`)
+    
+    if(isLoading)return <p className="">Loading movie...</p>
+    if(error)return <p className="">Error: {error.message}</p>
+    if(!movie)return <p className="">Movie not found</p>
+    
     return (
-        <>Movie Details Page</>
+        <div className="">
+            <button className="" onClick={()=> navigate(-1)}>← Back</button>
+        
+            <div className="">
+                <p className="">{movie.Title}</p>
+                <p className="">{movie.Year} • {movie.Genre} • {movie.Runtime}</p>
+            </div>
+        
+        </div>
     );
 }
  
