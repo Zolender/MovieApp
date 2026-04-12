d395a584e9a85523d279212780a372dc69cc2941
Zolender <ndeingare@gmail.com>
2026-04-12 22:38:49 +0200

    feat: add favorite reducer to the store configuration

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice"
+import favoriteReducer from "./favoriteSlice"
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        favorite: favoriteReducer
     }
 })
 
