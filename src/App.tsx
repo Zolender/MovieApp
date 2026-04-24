b76160cc45b70a8cf6c1bef2f7f1cca308df5f8e
Zolender <ndeingare@gmail.com>
2026-04-24 18:04:48 +0200

    feat: implement theme toggle functionality and enhance styling for light and dark modes

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,35 @@ import PageNotFound from "./Pages/PageNotFound"
 import PublicRoute from "./components/PublicRoute"
 import { Toaster } from "react-hot-toast"
 import ErrorBoundary from "./components/errorBoundary"
+import { useSelector } from "react-redux"
+import { RootState } from "./store/store"
 function App() {
+  const theme = useSelector((state: RootState)=> state.theme.theme)
 
   return (
-    <BrowserRouter>
-      <Toaster position="bottom-right"/>
+    <div data-theme={theme} style={{minHeight : "100vh"}}>
+      <BrowserRouter>
+        <Toaster position="bottom-right"/>
 
-      <ErrorBoundary>
-        <Routes>
-          <Route element = {<PublicRoute/>}>
-            <Route path="/login" element={<Login/>}/>
-          </Route>
+        <ErrorBoundary>
+          <Routes>
+            <Route element = {<PublicRoute/>}>
+              <Route path="/login" element={<Login/>}/>
+            </Route>
 
-          
-          <Route element={<ProtectedRoute/>}>
-            <Route path="/" element={<Navigate to="/search" replace/>}/>
-            <Route path="/search" element={<Search/>}/>
-            <Route path="/movies/:id" element={<MovieDetails/>}/>
-            <Route path="/favorites" element={<Favorites/>}/>
-          </Route>
+            
+            <Route element={<ProtectedRoute/>}>
+              <Route path="/" element={<Navigate to="/search" replace/>}/>
+              <Route path="/search" element={<Search/>}/>
+              <Route path="/movies/:id" element={<MovieDetails/>}/>
+              <Route path="/favorites" element={<Favorites/>}/>
+            </Route>
 
-          <Route path="*" element={<PageNotFound/>}/>
-        </Routes>
-      </ErrorBoundary>
-    </BrowserRouter>
+            <Route path="*" element={<PageNotFound/>}/>
+          </Routes>
+        </ErrorBoundary>
+      </BrowserRouter>
+    </div>
   )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,17 +2,23 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { RootState } from "../store/store";
 import { logoutUser } from "../store/authSlice";
+import { Moon, Sun } from "lucide-react";
+import { toggleTheme } from "../store/themeSlice";
 
 const NavBar = () => {
 
     const dispatch = useDispatch()
     const currentUser = useSelector((state: RootState)=> state.auth.currentUser)
     const navigate = useNavigate()
+    const theme = useSelector((state: RootState)=> state.theme.theme)
 
     function handleLogout(){
         dispatch(logoutUser())
         navigate("/login", {replace: true})
 
+    }
+    function handleToggle (){
+        dispatch(toggleTheme())
     }
 
     return (
@@ -21,6 +27,7 @@ const NavBar = () => {
                 
                 <div className="flex items-center space-x-4">
                     <p className="text-xl font-bold">Welcome, <span className="">{currentUser?.username || "User"}</span></p>
+                    <button className="transition-all duration-300 ease-in-out hover:cursor-pointer" onClick={handleToggle}>{theme=== "light"? <Moon/> : <Sun/> }</button>
                 </div>
                 <div className="flex items-center space-x-4">
                     <Link to={"/search"} className="text-blue-500 hover:underline">Search</Link>
